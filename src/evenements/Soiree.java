7d52303303c1ebcb8d0e53a54ce0d3d90221195e
lukile <lucile.1988.ls@gmail.com>
2017-06-02 09:51:24 +0200

    clean up

--- src/evenements/Soiree.java
+++ src/evenements/Soiree.java
@@ -4,48 +4,15 @@ import java.sql.Date;
 import java.sql.Time;
 
 public class Soiree extends Evenement{
-	private int 	id;
 	private Time 	heureDebutSoiree;
 	private Time	heureFinSoiree;
 	
-	public Soiree(){}
-	
-	public Soiree(
-			int id,
-			int numeroIdentification,
-			String nom,
-			String prenom,
-			String email, 
-			String telephone,
-			int codeProjet,
-			Date dateEmbauche,
-			String[] ville,
-			Double salaire,
-			Double prime,
-			boolean essaiClinique,
-			Date debutEssaiClinique,
-			Date finEssaiClinique,
-			String moleculeTestee,
-			Date dateEvenement,
-			Time heureDebutSoiree,
-			Time heureFinSoiree){
+	public Soiree(Date dateEvenement, Time heureDebutSoiree, Time heureFinSoiree){
 		
-		super(id, numeroIdentification, nom, prenom,
-				email, telephone, codeProjet, dateEmbauche,
-				ville, salaire, prime, essaiClinique, 
-				debutEssaiClinique, finEssaiClinique, moleculeTestee, dateEvenement);
+		super(dateEvenement);
 		
-		this.id = id;
 		this.heureDebutSoiree = heureDebutSoiree;
 		this.heureFinSoiree = heureFinSoiree;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public Time getHeureDebutSoiree() {
