369c6f07a2d77bd00d2df3ce630809c2742900a7
lukile <lucile.1988.ls@gmail.com>
2017-06-17 23:49:30 +0200

    updated DB, new table commercial_evenement

--- sql/sntlabo.sql
+++ sql/sntlabo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 17 Juin 2017 à 18:18
+-- Généré le :  Sam 17 Juin 2017 à 21:48
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -42,18 +42,8 @@ CREATE TABLE `collaborateur` (
 --
 
 INSERT INTO `collaborateur` (`numeroIdentification`, `nom`, `prenom`, `email`, `telephone`, `codeProjet`, `dateEmbauche`, `ville`) VALUES
-(72, 'COTTIN', 'Jean paul', 'jep@gm.com', '2020020202', 1, '25/03/2012', 'cottinet'),
-(73, 'POUETOU', 'Jean Louis', 'jl@pouetou.com', '20020202', 2, '24/03/2012', 'POUETOU CHARENTE'),
-(74, 'POUICOU', 'Jean Luc', 'jl@pouicou.com', '020202020', 3, '23/09/2300', 'POUICOU SUR LOIRE'),
-(75, 'COCO LE CONNARD', 'Stipe', 'cocostipé@g.com', '3333333', 4, '21/09/3210', 'COCOUNET'),
-(76, 'ee', 'ee', 'ee', 'ee', 1, 'EE', 'EE'),
-(77, 'a', 'a', 'a', 'a', 1, 'A', 'a'),
-(78, 'pouet', 'pouet', 'pouet', 'é', 3, 'e', 'e'),
-(79, 'd', 'd', 'd', 'd', 2, 'd', 'd'),
-(80, 'p', 'p', 'p', 'p', 1, 'p', 'p'),
-(81, 'a', 'a', 'a', 'a', 1, 'A', 'A'),
-(82, 'a', 'a', 'a', 'a', 1, 'a', '1'),
-(83, 'A', 'a', 'a', '1', 1, '1', '1');
+(95, 'COCO', 'jean charles', 'jc@g.com', '220220', 3, '12/02/2012', 'Pari'),
+(96, 'POUET', 'POUET', 'pouet', '12200202', 3, 'kkeke', 'ekkee');
 
 -- --------------------------------------------------------
 
@@ -73,7 +63,27 @@ CREATE TABLE `commercial` (
 --
 
 INSERT INTO `commercial` (`nIdentification`, `salaire`, `noteDeFrais`, `remboursement`) VALUES
-(75, 4000, 211, 1);
+(95, 12000, 1493, 1),
+(96, 12, 14, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commercial_evenement`
+--
+
+CREATE TABLE `commercial_evenement` (
+  `idCommercial` int(11) NOT NULL,
+  `idEvenement` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `commercial_evenement`
+--
+
+INSERT INTO `commercial_evenement` (`idCommercial`, `idEvenement`) VALUES
+(95, 6),
+(96, 7);
 
 -- --------------------------------------------------------
 
@@ -83,8 +93,8 @@ INSERT INTO `commercial` (`nIdentification`, `salaire`, `noteDeFrais`, `rembours
 
 CREATE TABLE `congres` (
   `idEvenement` int(11) NOT NULL,
-  `dateDebutCongres` date DEFAULT NULL,
-  `dateFinCongres` date DEFAULT NULL
+  `dateDebutCongres` varchar(255) DEFAULT NULL,
+  `dateFinCongres` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -98,6 +108,14 @@ CREATE TABLE `evenement` (
   `adresse` varchar(255) DEFAULT NULL,
   `moleculeTestee` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `evenement`
+--
+
+INSERT INTO `evenement` (`id`, `adresse`, `moleculeTestee`) VALUES
+(6, '3 rue de la bigotiere', 'chavroux'),
+(7, 'ZEEEO', 'ekek');
 
 -- --------------------------------------------------------
 
@@ -113,13 +131,6 @@ CREATE TABLE `medecin` (
   `debutEssaiClinique` varchar(255) DEFAULT NULL,
   `finEssaiClinique` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `medecin`
---
-
-INSERT INTO `medecin` (`nIdentification`, `salaire`, `prime`, `essaiClinique`, `debutEssaiClinique`, `finEssaiClinique`) VALUES
-(72, 2500, 250, 1, '12/12/2012', '12/01/2013');
 
 -- --------------------------------------------------------
 
@@ -145,22 +156,6 @@ CREATE TABLE `scientifique` (
   `responsable` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `scientifique`
---
-
-INSERT INTO `scientifique` (`nIdentification`, `salaire`, `prime`, `responsable`) VALUES
-(73, 3000, 250, 1),
-(74, 3000, 210, 0),
-(76, 12, 12, 1),
-(77, 1, 1, 1),
-(78, 1, 1, 1),
-(79, 1, 1, 1),
-(80, 1, 1, 1),
-(81, 1, 1, 0),
-(82, 1, 1, 0),
-(83, 1, 1, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -169,9 +164,17 @@ INSERT INTO `scientifique` (`nIdentification`, `salaire`, `prime`, `responsable`
 
 CREATE TABLE `soiree` (
   `idEvenement` int(11) NOT NULL,
+  `dateSoiree` varchar(255) NOT NULL,
   `heureDebutSoiree` time DEFAULT NULL,
   `heureFinSoiree` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `soiree`
+--
+
+INSERT INTO `soiree` (`idEvenement`, `dateSoiree`, `heureDebutSoiree`, `heureFinSoiree`) VALUES
+(7, '12/02/2122', '12:00:00', '23:00:00');
 
 -- --------------------------------------------------------
 
@@ -191,17 +194,6 @@ CREATE TABLE `unite` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `unite`
---
-
-INSERT INTO `unite` (`id`, `nomUnite`, `numeroRue`, `nomRue`, `codePostal`, `villeUnite`, `dateResponsabilite`, `nIdentificationSC`) VALUES
-(1, 'Unite centrale', '3bis', 'rue de la Girouette', 74321, 'Rouen', '12/07/2012', NULL),
-(2, 'Unite centrale', '3bis', 'rue de la Girouette', 74321, 'Rouen', '12/07/2012', NULL),
-(3, 'Unite centrale', '3bis', 'rue de la Girouette', 74321, 'Rouen', '12/07/2012', 80),
-(4, 'pouet', '1', 'pouet', 12221, '', '0', 82),
-(5, 'pouet', '12', '12', 12222, '12', '12/12/2012', 83);
-
---
 -- Index pour les tables exportées
 --
 
@@ -217,6 +209,13 @@ ALTER TABLE `collaborateur`
 ALTER TABLE `commercial`
   ADD PRIMARY KEY (`nIdentification`),
   ADD KEY `nIdentification` (`nIdentification`);
+
+--
+-- Index pour la table `commercial_evenement`
+--
+ALTER TABLE `commercial_evenement`
+  ADD PRIMARY KEY (`idCommercial`,`idEvenement`),
+  ADD KEY `idEvenement` (`idEvenement`);
 
 --
 -- Index pour la table `congres`
@@ -242,6 +241,7 @@ ALTER TABLE `medecin`
 -- Index pour la table `medecin_evenement`
 --
 ALTER TABLE `medecin_evenement`
+  ADD PRIMARY KEY (`idMedecin`,`idEvenement`),
   ADD KEY `idMedecin` (`idMedecin`),
   ADD KEY `idEvenement` (`idEvenement`);
 
@@ -273,12 +273,12 @@ ALTER TABLE `unite`
 -- AUTO_INCREMENT pour la table `collaborateur`
 --
 ALTER TABLE `collaborateur`
-  MODIFY `numeroIdentification` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `numeroIdentification` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 --
 -- AUTO_INCREMENT pour la table `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `unite`
 --
@@ -293,6 +293,14 @@ ALTER TABLE `unite`
 --
 ALTER TABLE `commercial`
   ADD CONSTRAINT `commercial_ibfk_1` FOREIGN KEY (`nIdentification`) REFERENCES `collaborateur` (`numeroIdentification`);
+
+--
+-- Contraintes pour la table `commercial_evenement`
+--
+ALTER TABLE `commercial_evenement`
+  ADD CONSTRAINT `commercial_evenement_ibfk_1` FOREIGN KEY (`idCommercial`) REFERENCES `commercial` (`nIdentification`),
+  ADD CONSTRAINT `commercial_evenement_ibfk_2` FOREIGN KEY (`idEvenement`) REFERENCES `evenement` (`id`),
+  ADD CONSTRAINT `commercial_evenement_ibfk_3` FOREIGN KEY (`idCommercial`) REFERENCES `commercial` (`nIdentification`);
 
 --
 -- Contraintes pour la table `congres`
