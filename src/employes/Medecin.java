e867ef6e8bef236af3eb8584169de8d9d449b052
lukile <lucile.1988.ls@gmail.com>
2017-06-19 00:05:12 +0200

    change constructor

--- src/employes/Medecin.java
+++ src/employes/Medecin.java
@@ -2,41 +2,43 @@ package employes;
 
 public class Medecin extends Collaborateur{
 
-	private Double 				salaire;
 	private Double 				prime;
 	private boolean 			essaiClinique;
 	private String 				debutEssaiClinique;
 	private String 				finEssaiClinique;
-	
+
 	public Medecin(
 			String nom,
-			String prenom, 
+			String prenom,
 			String email,
 			String telephone,
 			int codeProjet,
 			String dateEmbauche,
-			String ville, 
-			Double salaire, 
-			Double prime, 
-			boolean essaiClinique, 
+			String ville,
+			Double prime,
+			boolean essaiClinique,
 			String debutEssaiClinique,
 			String finEssaiClinique){
-		
+
 		super(nom, prenom, email, telephone, codeProjet, dateEmbauche, ville);
-		
-		this.salaire = salaire;
+
 		this.prime = prime;
 		this.essaiClinique = essaiClinique;
 		this.debutEssaiClinique = debutEssaiClinique;
 		this.finEssaiClinique = finEssaiClinique;
 	}
 
-	public Double getSalaire() {
-		return salaire;
-	}
+	public Medecin(Collaborateur collaborateur,
+				   Double prime,
+				   boolean essaiClinique,
+				   String debutEssaiClinique,
+				   String finEssaiClinique){
 
-	public void setSalaire(Double salaire) {
-		this.salaire = salaire;
+		this(collaborateur.getNom(), collaborateur.getPrenom(), collaborateur.getEmail(),
+				collaborateur.getTelephone(), collaborateur.getCodeProjet(), collaborateur.getDateEmbauche(),
+				collaborateur.getVille(), prime, essaiClinique, debutEssaiClinique, finEssaiClinique);
+
+		this.setNumeroIdentification(collaborateur.getNumeroIdentification());
 	}
 
 	public Double getPrime() {
@@ -70,18 +72,18 @@ public class Medecin extends Collaborateur{
 	public void setFinEssaiClinique(String finEssaiClinique) {
 		this.finEssaiClinique = finEssaiClinique;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Medecin : "
 				+ super.toString()
-				+ ", salaire =" + salaire 
-				+ ", prime =" + prime 
+				+ ", salaire =" + salaire
+				+ ", prime =" + prime
 				+ ", essai Clinique =" + essaiClinique
-				+ ", date du d�but Essai Clinique = " + debutEssaiClinique 
+				+ ", date du d�but Essai Clinique = " + debutEssaiClinique
 				+ ", date de fin Essai Clinique =" + finEssaiClinique;
 	}
-	
-	
-	
+
+
+
 }
